3901c50fe786b3210be315ac117ac6e778ce90ae
vladislavzyatkovskiy <zyatkovskyy@gmail.com>
2023-04-17 12:20:39 +0600

    models added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
+
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    text = models.TextField()
+
+class Slider(models.Model):
+    title1 = models.CharField(max_length=255)
+    title2 = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='img')
+
