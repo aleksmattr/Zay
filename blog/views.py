d9371523cfeef6a5e35651e143d720f77d2f37f8
Александр Слепцов <aleksandrslepcov@MacBook-Air-Aleksandr.local>
2023-04-17 11:29:37 +0600

    all

---
+++ blog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def shop(request):
+    return render(request, 'shop.html')
+
+
+def shopsingle(request):
+    return render(request, 'shop-single.html')
